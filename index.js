418d5de7f09cf8cd934e61f2f018745af1da6e73
Rushabh Gothi <rushg171.gmail.com>
2020-03-19 16:45:31 +0530

    acao

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(indexRouter);
 app.use(cors());
-app.use(res.header("Access-Control-Allow-Origin", "*"));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 app.listen(PORT, () => console.log("Server Started!"));
